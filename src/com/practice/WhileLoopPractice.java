488a043e223238bc027a515ec9ecd0ccb3ae0e05
Your Name <gowthuchikku@gmail.com>
2025-05-10 18:03:40 +0530

    20250510 Loops Concepts

---
+++ src/com/practice/WhileLoopPractice.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+public class WhileLoopPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		while(i<20)
+		{
+			i++;
+			System.out.println(i);
+		}
+
+	}
+
+}
